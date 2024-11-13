9a421ecfed5c9a98bccd110bab63fe1bd33a4676
Gabriel Pereira <gabriel.brown1112@gmail.com>
2024-11-13 18:12:49 -0300

    tentativa de paralelizar (não funcionou)

--- FinalProject/tsp/tsp.c
+++ FinalProject/tsp/tsp.c
@@ -1,5 +1,5 @@
-/* WSCAD - 9th Marathon of Parallel Programming 
- * Simple Brute Force Algorithm for the 
+/* WSCAD - 9th Marathon of Parallel Programming
+ * Simple Brute Force Algorithm for the
  * Traveling-Salesman Problem
  * Author: Emilio Francesquini - francesquini@ic.unicamp.br
  */
@@ -8,11 +8,14 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <math.h>
+#include <sys/time.h>
+
 
 int min_distance;
 int nb_towns;
 
-typedef struct {
+typedef struct
+{
     int to_town;
     int dist;
 } d_info;
@@ -28,10 +31,12 @@ int *dist_to_origin;
  * @param path An array representing the sequence of towns visited so far.
  * @return 1 if the town is present in the path, otherwise 0.
  */
-int present (int town, int depth, int *path) {
+int present(int town, int depth, int *path)
+{
     int i;
     for (i = 0; i < depth; i++)
-        if (path[i] == town) return 1;
+        if (path[i] == town)
+            return 1;
     return 0;
 }
 
@@ -48,22 +53,29 @@ int present (int town, int depth, int *path) {
  * @param path An array of length nb_towns to store the sequence of towns
  *             visited so far.
  */
-void tsp (int depth, int current_length, int *path) {
+void tsp(int depth, int current_length, int *path)
+{
     int i;
-    if (current_length >= min_distance) return;
-    if (depth == nb_towns) {
+    if (current_length >= min_distance)
+        return;
+    if (depth == nb_towns)
+    {
         current_length += dist_to_origin[path[nb_towns - 1]];
         if (current_length < min_distance)
             min_distance = current_length;
-    } else {
+    }
+    else
+    {
         int town, me, dist;
         me = path[depth - 1];
-        for (i = 0; i < nb_towns; i++) {
+        for (i = 0; i < nb_towns; i++)
+        {
             town = d_matrix[me][i].to_town;
-            if (!present (town, depth, path)) {
+            if (!present(town, depth, path))
+            {
                 path[depth] = town;
                 dist = d_matrix[me][i].dist;
-                tsp (depth + 1, current_length + dist, path);
+                tsp(depth + 1, current_length + dist, path);
             }
         }
     }
@@ -80,32 +92,38 @@ void tsp (int depth, int current_length, int *path) {
  * @param x The x coordinates of each town.
  * @param y The y coordinates of each town.
  */
-void greedy_shortest_first_heuristic(int *x, int *y) {
+void greedy_shortest_first_heuristic(int *x, int *y)
+{
     int i, j, k, dist;
     int *tempdist;
 
-    tempdist = (int*) malloc(sizeof(int) * nb_towns);
-    //Could be faster, albeit not as didactic.
-    //Anyway, for tractable sizes of the problem it
-    //runs almost instantaneously.
-    for (i = 0; i < nb_towns; i++) {
-        for (j = 0; j < nb_towns; j++) {
+    tempdist = (int *)malloc(sizeof(int) * nb_towns);
+    // Could be faster, albeit not as didactic.
+    // Anyway, for tractable sizes of the problem it
+    // runs almost instantaneously.
+    for (i = 0; i < nb_towns; i++)
+    {
+        for (j = 0; j < nb_towns; j++)
+        {
             int dx = x[i] - x[j];
             int dy = y[i] - y[j];
-            tempdist [j] = dx * dx + dy * dy;
+            tempdist[j] = dx * dx + dy * dy;
         }
-        for (j = 0; j < nb_towns; j++) {
+        for (j = 0; j < nb_towns; j++)
+        {
             int tmp = INT_MAX;
             int town = 0;
-            for (k = 0; k < nb_towns; k++) {
-                if (tempdist [k] < tmp) {
-                    tmp = tempdist [k];
+            for (k = 0; k < nb_towns; k++)
+            {
+                if (tempdist[k] < tmp)
+                {
+                    tmp = tempdist[k];
                     town = k;
                 }
             }
-            tempdist [town] = INT_MAX;
+            tempdist[town] = INT_MAX;
             d_matrix[i][j].to_town = town;
-            dist = (int) sqrt (tmp);
+            dist = (int)sqrt(tmp);
             d_matrix[i][j].dist = dist;
             if (i == 0)
                 dist_to_origin[town] = dist;
@@ -125,31 +143,34 @@ void greedy_shortest_first_heuristic(int *x, int *y) {
  *
  * @return void
  */
-void init_tsp() {
+void init_tsp()
+{
     int i, st;
     int *x, *y;
 
     min_distance = INT_MAX;
 
     st = scanf("%u", &nb_towns);
-    if (st != 1) exit(1);
- 
-    d_matrix = (d_info**) malloc (sizeof(d_info*) * nb_towns);
-    for (i = 0; i < nb_towns; i++)
-        d_matrix[i] = (d_info*) malloc (sizeof(d_info) * nb_towns);
-    dist_to_origin = (int*) malloc(sizeof(int) * nb_towns);
-   
-    x = (int*) malloc(sizeof(int) * nb_towns);
-    y = (int*) malloc(sizeof(int) * nb_towns);
-    
+    if (st != 1)
+        exit(1);
 
-    for (i = 0; i < nb_towns; i++) {
+    d_matrix = (d_info **)malloc(sizeof(d_info *) * nb_towns);
+    for (i = 0; i < nb_towns; i++)
+        d_matrix[i] = (d_info *)malloc(sizeof(d_info) * nb_towns);
+    dist_to_origin = (int *)malloc(sizeof(int) * nb_towns);
+
+    x = (int *)malloc(sizeof(int) * nb_towns);
+    y = (int *)malloc(sizeof(int) * nb_towns);
+
+    for (i = 0; i < nb_towns; i++)
+    {
         st = scanf("%u %u", x + i, y + i);
-        if (st != 2) exit(1);
+        if (st != 2)
+            exit(1);
     }
-    
+
     greedy_shortest_first_heuristic(x, y);
-    
+
     free(x);
     free(y);
 }
@@ -164,21 +185,26 @@ void init_tsp() {
  *
  * @return The minimum distance of the routes found.
  */
-int run_tsp() {
+int run_tsp()
+{
     int i, *path;
-
+    struct timeval start, stop;
     init_tsp();
-    
-    path = (int*) malloc(sizeof(int) * nb_towns);
+     gettimeofday(&start, NULL);
+    path = (int *)malloc(sizeof(int) * nb_towns);
     path[0] = 0;
-    
-    tsp (1, 0, path);
+
+    tsp(1, 0, path);
 
     free(path);
     for (i = 0; i < nb_towns; i++)
         free(d_matrix[i]);
     free(d_matrix);
+    gettimeofday(&stop, NULL); 
+    double t = (((double)(stop.tv_sec)*1000.0  + (double)(stop.tv_usec / 1000.0)) - \
+                   ((double)(start.tv_sec)*1000.0 + (double)(start.tv_usec / 1000.0)));
 
+  fprintf(stdout, "Tempo decorrido = %g ms\n", t);
     return min_distance;
 }
 
@@ -194,11 +220,13 @@ int run_tsp() {
  * @param argv The command line arguments.
  * @return 0 if successful, -1 otherwise.
  */
-int main (int argc, char **argv) {
+int main(int argc, char **argv)
+{
     int num_instances, st;
     st = scanf("%u", &num_instances);
-    if (st != 1) exit(1);
-    while(num_instances-- > 0)
+    if (st != 1)
+        exit(1);
+    while (num_instances-- > 0)
         printf("%d\n", run_tsp());
     return 0;
 }
